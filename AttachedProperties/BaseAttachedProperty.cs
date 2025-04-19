708868a00f63fbaba4a813c727c62f43ca2b53f8
Ihor Pavlov <Ihor.Pavlov@outlook.com>
2025-04-20 01:47:01 +0300

    Refactor PasswordBox attached properties and bindings
    This commit replaces the `PasswordBoxProperties` class with two new classes: `MonitorPasswordProperty` and `HasTextProperty`, inheriting from a new base class `BaseAttachedProperty`. This refactoring enhances the structure and reusability of the attached properties.
    
    In `Texts.xaml`, the `MonitorPassword` property binding has been updated to reflect the new class structure, and the `HasText` property now utilizes a converter for visibility. Additionally, a new `BooleanToVisibilityConverter` class has been introduced to manage visibility states based on the `PasswordBox` content.
    
    These changes improve code maintainability and clarity.

---
+++ AttachedProperties/BaseAttachedProperty.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows;
+
+namespace FasettoWord.AttachedProperties
+{
+    /// <summary>
+    /// Base class for all attached properties to replace the vanilla WPF attached property
+    /// </summary>
+    /// <typeparam name="Parent">The parent class to be the attached property</typeparam>
+    /// <typeparam name="Property">The type of this attached property</typeparam>
+    public abstract class BaseAttachedProperty<Parent, Property>
+        where Parent : BaseAttachedProperty<Parent, Property>, new()
+    {
+        #region Events
+        /// <summary>
+        /// The event that is raised when the attached property changes
+        /// </summary>
+        public event Action<DependencyObject, DependencyPropertyChangedEventArgs> ValueChanged;
+        #endregion
+
+        #region Instance
+        /// <summary>
+        /// Singleton instance of this class
+        /// </summary>
+        public static Parent Instance { get; } = new();
+        #endregion
+
+        #region Attached Property Definition
+        /// <summary>
+        /// The attached property for this class
+        /// </summary>
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.RegisterAttached(
+                "Value",
+                typeof(Property),
+                typeof(BaseAttachedProperty<Parent, Property>),
+                new PropertyMetadata(
+                    default(Property),
+                    new PropertyChangedCallback(OnValuePropertyChanged)));
+
+        /// <summary>
+        /// The callback event whet the <see cref="ValueProperty"/> changes"/>
+        /// </summary>
+        /// <param name="d">The UI element that hd it's property changed</param>
+        /// <param name="e">The argumentsfor the event</param>
+        /// <exception cref="NotImplementedException"></exception>
+        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Instance.OnValueChanged(d, e);
+            Instance.ValueChanged?.Invoke(d, e);
+        }
+        /// <summary>
+        /// Gets the attached property
+        /// </summary>
+        /// <param name="d">The element to get the property from</param>
+        /// <returns></returns>
+        public static Property GetValue(DependencyObject d) => (Property)d.GetValue(ValueProperty);
+        /// <summary>
+        /// Sets the attached property
+        /// </summary>
+        /// <param name="d">The element to get the property from</param>
+        /// <param name="value">The value to set the property to</param>
+        public static void SetValue(DependencyObject d, Property value) => d.SetValue(ValueProperty, value);
+        #endregion
+
+        #region Event Methods
+        public virtual void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) { }
+        #endregion
+    }
+}
---
+++ AttachedProperties/PasswordBoxAttachedProperties.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FasettoWord.AttachedProperties
+{
+    /// <summary>
+    /// The MonitorPassword attached property for the <see cref="PasswordBox"/>
+    /// </summary>
+    public class MonitorPasswordProperty : BaseAttachedProperty<MonitorPasswordProperty, bool>
+    {
+        public override void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PasswordBox passwordBox)
+            {
+                passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+                if ((bool)e.NewValue)
+                {
+                    HasTextProperty.SetValue(passwordBox);
+                    passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+                }
+            }
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e) => HasTextProperty.SetValue(sender as PasswordBox);
+    }
+
+    /// <summary>
+    /// The HasText attached property for the <see cref="PasswordBox"/>
+    /// </summary>
+    public class HasTextProperty : BaseAttachedProperty<HasTextProperty, bool> 
+    { 
+        public static void SetValue(PasswordBox passwordBox) => SetValue(passwordBox, passwordBox.SecurePassword.Length > 0);        
+    }
+}
